5a536854129ad38d76bbdfce442106adf6ad5145
Damian-portatil <damian.aguilarm@gmail.com>
2025-06-10 19:09:14 +0200

    working on...

--- src/BelichEnvManagerServiceProvider.php
+++ src/BelichEnvManagerServiceProvider.php
@@ -11,13 +11,11 @@ class BelichEnvManagerServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Registra la configuración del paquete si tienes un archivo config
-        // $this->mergeConfigFrom(__DIR__.'/../config/belich-env-manager.php', 'belich-env-manager');
+        $this->mergeConfigFrom(__DIR__.'/../config/belich-env-manager.php', 'belich-env-manager');
 
-        // Registra tus clases principales aquí si son singleton o necesitan binding
-        // $this->app->singleton(EnvManager::class, function ($app) {
-        //     return new EnvManager();
-        // });
+        $this->app->singleton(EnvManager::class, function ($app) {
+            return new EnvManager($app['files'], $app['config']);
+        });
     }
 
     /**
@@ -25,31 +23,9 @@ class BelichEnvManagerServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Publica archivos de configuración, vistas, migraciones, etc.
-        // if ($this->app->runningInConsole()) {
-        //     $this->publishes([
-        //         __DIR__.'/../config/belich-env-manager.php' => config_path('belich-env-manager.php'),
-        //     ], 'belich-env-manager-config');
-
-        //     $this->publishes([
-        //         __DIR__.'/../resources/views' => resource_path('views/vendor/belich-env-manager'),
-        //     ], 'belich-env-manager-views');
-
-        //     // Publica tus archivos de definición YAML/JSON aquí
-        //     $this->publishes([
-        //         __DIR__.'/../config/env-definitions.yaml' => config_path('env-definitions.yaml'),
-        //     ], 'belich-env-manager-definitions');
-
-        //     // Registra comandos de Artisan
-        //     // $this->commands([
-        //     //     YourEnvManagerCommand::class,
-        //     // ]);
-        // }
-
-        // Carga vistas
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'belich-env-manager');
-
-        // Carga rutas
-        // $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/belich-env-manager.php' => config_path('belich-env-manager.php'),
+            ], 'belich-env-manager-config');
     }
 }
